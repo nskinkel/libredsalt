37ba54ec16b5579c687fde24b80be2a4ff60f1a9
David Johnston <dwtj@iastate.edu>
2015-08-29 17:35:36 -0500

    Adds `crypto_box_keypair()`.

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,18 @@
+extern crate libc;
+
+mod ffi;
+
 #[test]
 fn it_works() {
-    // TODO: crypto_box_keypair();
+    let mut pk = [0u8; ffi::crypto_box_PUBLICKEYBYTES as usize];
+    let mut sk = [0u8; ffi::crypto_box_SECRETKEYBYTES as usize];
+    let ret = crypto_box_keypair(&mut pk, &mut sk);
+    println!("{}", ret);
+}
+
+pub fn crypto_box_keypair(pk: &mut[u8], sk: &mut[u8]) -> i32 {
+    // TODO: Assert correctly sized slices.
+    unsafe {
+        ffi::crypto_box_keypair(pk.as_mut_ptr(), sk.as_mut_ptr())
+    }
 }
