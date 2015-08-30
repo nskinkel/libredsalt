513bcacf088985ce9ad2593b708a4115e318c064
David Johnston <dwtj@iastate.edu>
2015-08-29 20:39:25 -0500

    Adds FFI function names. (Types still wrong.)
    Also:
    - Adds wrapper stubs.
    - Adds the rest of the integer symbolic constants.
    - Changed the type each integer of symbolic constant from `i32` to
      `usize`.

--- src/ffi.rs
+++ src/ffi.rs
@@ -3,16 +3,67 @@
 
 use libc::{size_t, c_int};
 
-pub const crypto_box_PUBLICKEYBYTES: i32 = 32;
-pub const crypto_box_SECRETKEYBYTES: i32 = 32;
-pub const crypto_box_NONCEBYTES: i32 = 24;
-pub const crypto_box_ZEROBYTES: i32 = 32;
-pub const crypto_box_BOXZEROBYTES: i32 = 16;
-pub const crypto_box_BEFORENMBYTES: i32 = 32;
+pub const crypto_auth_BYTES: usize = 32;
+pub const crypto_auth_KEYBYTES: usize = 32;
 
-#[link(name = "tweetnacl", kind = "static")]
+pub const crypto_box_PUBLICKEYBYTES: usize = 32;
+pub const crypto_box_SECRETKEYBYTES: usize = 32;
+pub const crypto_box_NONCEBYTES: usize = 24;
+pub const crypto_box_ZEROBYTES: usize = 32;
+pub const crypto_box_BOXZEROBYTES: usize = 16;
+pub const crypto_box_BEFORENMBYTES: usize = 32;
+
+pub const crypto_hash_BYTES: usize = 64;
+
+pub const crypto_onetimeauth_BYTES: usize = 16;
+pub const crypto_onetimeauth_KEYBYTES: usize = 32;
+
+pub const crypto_scalarmult_BYTES: usize = 32;
+pub const crypto_scalarmult_SCALARBYTES: usize = 32;
+
+pub const crypto_secretbox_KEYBYTES: usize = 32;
+pub const crypto_secretbox_NONCEBYTES: usize = 24;
+pub const crypto_secretbox_ZEROBYTES: usize = 32;
+pub const crypto_secretbox_BOXZEROBYTES: usize = 16;
+
+pub const crypto_sign_PUBLICKEYBYTES: usize = 32;
+pub const crypto_sign_SECRETKEYBYTES: usize = 64;
+pub const crypto_sign_BYTES: usize = 64;
+
+pub const crypto_stream_KEYBYTES: usize = 32;
+pub const crypto_stream_NONCEBYTES: usize = 24;
+
+
 extern {
-    //fn crypto_box_open(source_length: size_t) -> size_t;
-    //fn crypto_box_open(u8 *m, const u8 *c, u64 d, const u8 *n,const u8 *y,const u8 *x) -> c_int;
-    pub fn crypto_box_keypair(pk: *mut u8, sk: *mut u8) -> i32;
+    /*
+    pub fn crypto_verify_16_tweet(const u8 *x,const u8 *y) -> c_int;
+    pub fn crypto_verify_32_tweet(const u8 *x,const u8 *y) -> c_int;
+
+    pub fn crypto_stream_xsalsa20_tweet(u8 *c,u64 d,const u8 *n,const u8 *k);
+    pub fn crypto_stream_xsalsa20_tweet_xor(c,m,mlen,n,k);
+
+    pub fn crypto_onetimeauth_poly1305_tweet(u8 *out,const u8 *m,u64 n,const u8 *k) -> c_int;
+    pub fn crypto_onetimeauth_poly1305_tweet_verify(const u8 *h,const u8 *m,u64 n,const u8 *k) -> c_int;
+
+    pub fn crypto_scalarmult_curve25519_tweet(u8 *q,const u8 *n,const u8 *p) -> c_int;
+    pub fn crypto_scalarmult_curve25519_tweet_base(u8 *q,const u8 *n) -> c_int;
+    */
+
+    pub fn crypto_box_curve25519xsalsa20poly1305_tweet_keypair(pk: *mut u8, sk: *mut u8) -> c_int;
+    /*
+    pub fn crypto_box_curve25519xsalsa20poly1305_tweet_beforenm(u8 *k,const u8 *y,const u8 *x) -> c_int;
+    pub fn crypto_box_curve25519xsalsa20poly1305_tweet_afternm(u8 *c,const u8 *m,u64 d,const u8 *n,const u8 *k) -> c_int;
+    pub fn crypto_box_curve25519xsalsa20poly1305_tweet_open_afternm(u8 *m,const u8 *c,u64 d,const u8 *n,const u8 *k) -> c_int;
+    pub fn crypto_box_curve25519xsalsa20poly1305_tweet(u8 *c,const u8 *m,u64 d,const u8 *n,const u8 *y,const u8 *x) -> c_int;
+    pub fn crypto_box_curve25519xsalsa20poly1305_tweet_open(u8 *m,const u8 *c,u64 d,const u8 *n,const u8 *y,const u8 *x) -> c_int
+
+    pub fn crypto_secretbox_xsalsa20poly1305_tweet(u8 *c,const u8 *m,u64 d,const u8 *n,const u8 *k) -> c_int;
+    pub fn crypto_secretbox_xsalsa20poly1305_tweet_open(u8 *m,const u8 *c,u64 d,const u8 *n,const u8 *k) -> c_int;
+
+    pub fn crypto_hash_sha512_tweet(u8 *out,const u8 *m,u64 n) -> c_int;
+
+    pub fn crypto_sign_ed25519_tweet_keypair(u8 *pk, u8 *sk) -> c_int;
+    pub fn crypto_sign_ed25519_tweet(u8 *sm,u64 *smlen,const u8 *m,u64 n,const u8 *sk) -> c_int;
+    pub fn crypto_sign_ed25519_tweet_open(u8 *m,u64 *mlen,const u8 *sm,u64 n,const u8 *pk) -> c_int;
+    */
 }
--- src/lib.rs
+++ src/lib.rs
@@ -2,17 +2,134 @@ extern crate libc;
 
 mod ffi;
 
+
+pub fn crypto_verify_16() {
+    unsafe {
+        //ffi::crypto_verify_16()
+    }
+}
+
+pub fn crypto_verify_32() {
+    unsafe {
+        //ffi::crypto_verify_32();
+    }
+}
+
+pub fn crypto_stream() {
+    unsafe {
+        //ffi::crypto_steam();
+    }
+}
+
+pub fn crypto_stream_xor() {
+    unsafe {
+        //ffi::crypto_steam_xor();
+    }
+}
+
+pub fn crypto_onetimeauth() {
+    unsafe {
+        //ffi::crypto_onetimeauth();
+    }
+}
+
+pub fn crypto_onetimeauth_verify() {
+    unsafe {
+        //ffi::crypto_onetimeauth_verify();
+    }
+}
+
+pub fn crypto_scalarmult() {
+    unsafe {
+        //ffi::crypto_scalarmult();
+    }
+}
+
+pub fn crypto_scalarmult_base() {
+    unsafe {
+        //ffi::crypto_scalarmult_base();
+    }
+}
+
+
 #[test]
-fn it_works() {
-    let mut pk = [0u8; ffi::crypto_box_PUBLICKEYBYTES as usize];
-    let mut sk = [0u8; ffi::crypto_box_SECRETKEYBYTES as usize];
-    let ret = crypto_box_keypair(&mut pk, &mut sk);
-    println!("{}", ret);
+fn test_crypto_box_keypair() {
+    let mut pk = [0u8; ffi::crypto_box_PUBLICKEYBYTES];
+    let mut sk = [0u8; ffi::crypto_box_SECRETKEYBYTES];
+    println!("rv: {:?}", crypto_box_keypair(&mut pk, &mut sk));
+    println!("pk: {:?}", pk);
+    println!("sk: {:?}", sk);
 }
 
 pub fn crypto_box_keypair(pk: &mut[u8], sk: &mut[u8]) -> i32 {
     // TODO: Assert correctly sized slices.
     unsafe {
-        ffi::crypto_box_keypair(pk.as_mut_ptr(), sk.as_mut_ptr())
+        ffi::crypto_box_curve25519xsalsa20poly1305_tweet_keypair(pk.as_mut_ptr(), sk.as_mut_ptr())
+    }
+}
+
+pub fn crypto_box_beforenm() {
+    unsafe {
+        //ffi::crypto_box_beforenm();
+    }
+}
+
+pub fn crypto_box_afternm() {
+    unsafe {
+        //ffi::crypto_box_afternm();
+    }
+}
+
+pub fn crypto_box_open_afternm() {
+    unsafe {
+        //ffi::crypto_box_open_afternm();
+    }
+}
+
+pub fn crypto_box() {
+    unsafe {
+        //ffi::crypto_box();
+    }
+}
+
+pub fn crypto_box_open() {
+    unsafe {
+        //ffi::crypto_box_open();
+    }
+}
+
+pub fn crypto_secretbox() {
+    unsafe {
+        //ffi::crypto_secretbox();
+    }
+}
+
+pub fn crypto_secretbox_open() {
+    unsafe {
+        //ffi::crypto_secretbox_open();
+    }
+}
+
+pub fn crypto_hash() {
+    unsafe {
+        //ffi::crypto_hash();
+    }
+}
+
+pub fn crypto_sign_keypair() {
+    unsafe {
+        //ffi::crypto_sign_keypair();
+    }
+}
+
+pub fn crypto_sign() {
+    unsafe {
+        //ffi::crypto_sign();
+    }
+}
+
+pub fn crypto_sign_open() {
+    unsafe {
+        //ffi::crypto_sign_open();
     }
 }
