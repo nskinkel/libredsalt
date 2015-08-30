72b62a3a8eaddd92b274a2052fb004dfc6759959
Nik Kinkel <nskinkel@iastate.edu>
2015-08-29 20:45:26 -0500

    update build.rs

--- build.rs
+++ build.rs
@@ -1,8 +1,6 @@
 extern crate gcc;
 
 fn main() {
-    gcc::Config::new()
-            .file("src/randombytes.c")
-            .file("src/tweetnacl.c")
-            .compile("libtweetnacl.a"); 
+    gcc::compile_library("libtweetnacl.a",
+        &["src/randombytes.c", "src/tweetnacl.c"]);
 }
