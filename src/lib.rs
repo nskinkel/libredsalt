48754b4766d80705134b697dabee68a9cd8b8da5
David Johnston <dwtj@iastate.edu>
2015-08-30 01:16:10 -0500

    `crypto_verify_XX()` check lengths and ret bools.

--- src/lib.rs
+++ src/lib.rs
@@ -2,18 +2,44 @@ extern crate libc;
 
 mod ffi;
 
-
-pub fn crypto_verify_16(x: &[u8], y: &[u8]) {
+/// Returns true iff the two 16 byte strings are equivalent.
+///
+/// ```
+/// let x: [42u8; 16];
+/// let y: [42u8; 16];
+/// assert!(crypto_verify_16(x, y) == true);
+/// ```
+pub fn crypto_verify_16(x: &[u8], y: &[u8]) -> bool {
+    assert!(x.len() == 16, "`x` must be 16 bytes.");
+    assert!(y.len() == 16, "`y` must be 16 bytes.");
     unsafe {
-        ffi::crypto_verify_16_tweet(x.as_ptr(), y.as_ptr());
+        match ffi::crypto_verify_16_tweet(x.as_ptr(), y.as_ptr()) {
+            0 => true,
+            -1 => false,
+            _ => unreachable!("Unexpected value produced by `crypto_verify_16_tweet()`.")
+        }
     }
 }
 
-pub fn crypto_verify_32_tweet(x: &[u8], y: &[u8]) {
+/// Returns true iff the two 32 byte strings are equivalent.
+/// ```
+/// let x: [42u8; 32];
+/// let y: [42u8; 32];
+/// assert!(crypto_verify_32(x, y) == true);
+/// ```
+pub fn crypto_verify_32(x: &[u8], y: &[u8]) -> bool {
+    assert!(x.len() == 32, "`x` must be 32 bytes.");
+    assert!(y.len() == 32, "`y` must be 32 bytes.");
     unsafe {
-        ffi::crypto_verify_32_tweet(x.as_ptr(), y.as_ptr());
+        match ffi::crypto_verify_32_tweet(x.as_ptr(), y.as_ptr()) {
+            0 => true,
+            -1 => false,
+            _ => unreachable!("Unexpected value produced by `crypto_verify_32_tweet()`.")
+        }
     }
 }
+
+test
 
 pub fn crypto_stream(out: &mut[u8], d: u64, n: &[u8], k: &[u8]) {
     unsafe {
@@ -80,7 +106,7 @@ pub fn crypto_box_afternm(c: &mut[u8], m: &[u8], d: u64, n: &[u8],
                           k: &[u8]) {
     unsafe {
         ffi::crypto_box_curve25519xsalsa20poly1305_tweet_afternm(
-            c.as_mut_ptr(), m.as_ptr(), d, n.as_ptr(), k.as_ptr()         
+            c.as_mut_ptr(), m.as_ptr(), d, n.as_ptr(), k.as_ptr()
         );
     }
 }
