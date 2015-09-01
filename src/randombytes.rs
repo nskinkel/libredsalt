429b8595f82d218a31bcaf56611a9391f848f6b4
Nik Kinkel <nskinkel@iastate.edu>
2015-09-01 11:48:43 -0500

    mark entire randombytes function as unsafe
    Although randombytes, as currently implemented, is only meant to be
    called from the tweetnacl C code, marking the entire function unsafe
    ensures that calls from Rust code will be required to be wrapped in an
    unsafe block. Semantically, this makes sense because the inherent
    unsafety of the inputs makes any subsequent operations inside the
    function unsafe and unable to be checked for safety.

--- src/randombytes.rs
+++ src/randombytes.rs
@@ -2,13 +2,11 @@ use rand::{OsRng, Rng};
 use std::slice;
 
 #[no_mangle]
-pub extern fn randombytes(buf: *mut u8, nbytes: u64) {
+pub unsafe extern fn randombytes(buf: *mut u8, nbytes: u64) {
     assert!(!buf.is_null());
     let mut rng = OsRng::new().ok().expect("OSRNG failed.");
-    unsafe {
-        let mut bytes = slice::from_raw_parts_mut(buf, nbytes as usize);
-        rng.fill_bytes(bytes);
-    }
+    let mut bytes = slice::from_raw_parts_mut(buf, nbytes as usize);
+    rng.fill_bytes(bytes);
 }
 
 
