fceb1284872c43885043682dcf232b47d07ef5e1
Nik Kinkel <nskinkel@iastate.edu>
2015-09-06 00:18:26 -0500

    add wrappers for crypto_onetimeauth
    Follows the NaCL c++ API style.

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@ extern crate rand;
 
 mod ffi;
 
+pub mod crypto_onetimeauth;
 pub mod randombytes;
 
 pub fn crypto_verify_16(x: &[u8], y: &[u8]) {
@@ -26,18 +27,6 @@ pub fn crypto_stream(out: &mut[u8], d: u64, n: &[u8], k: &[u8]) {
 pub fn crypto_stream_xor(c: &mut[u8], m: &[u8], d: u64, n: &[u8], k: &[u8]) {
     unsafe {
         ffi::crypto_stream_xsalsa20_tweet_xor(c.as_mut_ptr(), m.as_ptr(), d, n.as_ptr(), k.as_ptr());
-    }
-}
-
-pub fn crypto_onetimeauth(out: &mut[u8], m: &[u8], n: u64, k: &[u8]) {
-    unsafe {
-        ffi::crypto_onetimeauth_poly1305_tweet(out.as_mut_ptr(), m.as_ptr(), n, k.as_ptr());
-    }
-}
-
-pub fn crypto_onetimeauth_verify(h: &[u8], m: &[u8], n: u64, k: &[u8]) {
-    unsafe {
-        ffi::crypto_onetimeauth_poly1305_tweet_verify(h.as_ptr(), m.as_ptr(), n, k.as_ptr());
     }
 }
 
