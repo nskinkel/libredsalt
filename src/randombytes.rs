a64dbc6236b9e88a6398d5dc83cfbe4e8317ba35
Nik Kinkel <nskinkel@iastate.edu>
2015-09-10 11:45:41 -0500

    remove commented-out code

--- src/randombytes.rs
+++ src/randombytes.rs
@@ -10,9 +10,6 @@ lazy_static! {
 #[no_mangle]
 pub unsafe extern fn randombytes(buf: *mut u8, nbytes: u64) {
     assert!(!buf.is_null());
-    //let mut rng = OsRng::new().ok().expect("OSRNG failed.");
-    //let mut bytes = slice::from_raw_parts_mut(buf, nbytes as usize);
-    //rng.fill_bytes(bytes);
     RNG.lock().unwrap().fill_bytes(
         slice::from_raw_parts_mut(buf, nbytes as usize)
     );
